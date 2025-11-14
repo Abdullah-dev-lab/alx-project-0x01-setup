12eb1566fd57270ef3fa7f774104456cb3362606
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-11-14 01:22:53 +0100

    include setModalOpen

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -6,7 +6,7 @@ import PostModal from "@/components/common/PostModal";
 import { useState } from "react";
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setModalOpen] = useState(false);
     const [post, setPost] = useState<PostData | null>(null);
 
         const handleAddPost = (newPost: PostData) => {
@@ -20,7 +20,7 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
         <div className="flex justify-between">
         <h1 className=" text-2xl font-semibold">Post Content</h1>
         <button
-         onClick={() => setIsModalOpen(true)}
+         onClick={() => setModalOpen(true)}
          className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
@@ -33,7 +33,7 @@ const Posts: React.FC<PostProps[]> = ({ posts }) => {
       </main>
 
         {isModalOpen && (
-        <PostModal onClose={() => setIsModalOpen(false)} onSubmit={handleAddPost} />
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
         )}
     </div>
   )
