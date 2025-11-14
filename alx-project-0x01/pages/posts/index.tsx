c9370e832d88e897f755b8b67084a56e1145530c
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-11-14 01:15:11 +0100

    setup a Next.js Application

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,53 @@
+import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interfaces";
+import PostModal from "@/components/common/PostModal";
+import { useState } from "react";
+
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [post, setPost] = useState<PostData | null>(null);
+
+        const handleAddPost = (newPost: PostData) => {
+        setPost({ ...newPost, id: posts.leghth + 1 });
+};
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button
+         onClick={() => setIsModalOpen(true)}
+         className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
+      </main>
+
+        {isModalOpen && (
+        <PostModal onClose={() => setIsModalOpen(false)} onSubmit={handleAddPost} />
+        )}
+    </div>
+  )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
